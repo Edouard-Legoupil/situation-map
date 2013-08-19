69fc6656b06927d508a270008d74856ed4da0410
Edouard-Legoupil <edouard.legoupil@gmail.com>
2013-08-19 11:27:22 +0300

    customise for Jordan

--- js/site.js
+++ js/site.js
@@ -6,41 +6,22 @@ $(function() {
     // If MapBox layer: use map: 
     // Use tooltip: to add interactivity, set tooltip below at var = tooltip
     var layers = [
+
             {
-                title: 'Refugee Populations',
-                data: 'data/unhcr-refugees-bysettlement-mali.geojson',
-                scale: 'totalrefpop',
-                tooltip: 'population',
-                active: true /* Sets this layer as the default layer. */
-            },
-            {
-                title: 'Settlement Locations',
-                data: 'data/unhcr-refugees-bysettlement-mali.geojson',
-                tooltip: 'population'
-            },
-            {
-                title: 'Relief Activities',
-                data: 'data/unhcr-relief-3w-mali.geojson',
-                scale: 'num_partners',
-                tooltip: 'relief'
-            },
-            {
-                title: 'Population Flow',
+                title: 'School',
                 map: 'unhcr.pop_flow'
             },
             {
-                title: 'Border Crossings',
-                data: 'data/unhcr-infrastructure-border-crossing-mali.geojson',
-                tooltip: 'infrastructure'
+                title: 'Health Center',
+                map: 'unhcr.infra-jordan'
             },
             {
-                title: 'Infrastructure',
-                data: null, /* Set to null if there is no data available */
-                tooltip: 'infrastructure'
+                title: 'Hospital',
+                map: 'unhcr.infrajordanhospital'
             },
             {
                 title: 'UNHCR Offices',
-                map: 'unhcr.unhcr-global-offices'
+                map: 'unhcr.UNHCR-Offices'
             }
         ],
         basemap = {  
@@ -86,10 +67,10 @@ $(function() {
         m.addLayer(mapbox.layer().id(basemap.terrain));
         m.addLayer(mapbox.layer().id(borders.un).composite(false));
         (embed) ? m.zoom(5) : m.zoom(5);
-        m.center({ lat: 15, lon: -5 }).setZoomRange(2,12); /* Set center and zoom range */
+        m.center({ lat: 31.59809, lon: 36.36304 }).setZoomRange(7,13); /* Set center and zoom range */
         m.ui.zoomer.add();
         m.ui.attribution.add()
-            .content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');
+            .content('This map was produced as a reference aid only. The boundaries and names shown and the designations used on this map do not imply official endorsement or acceptance by the United Nations.');
         
         $.each(layer, function(i,l) {
             m.addLayer(l.layer.composite(false));
